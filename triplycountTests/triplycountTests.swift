bc7ee6a1362b02d6b75317a397b71e9543511903
Paul Martinez <paulmartinez@hotmail.fr>
2025-04-13 18:20:02 +0200

    Initial commit

---
+++ triplycountTests/triplycountTests.swift
@@ -0,0 +1,17 @@
+//
+//  triplycountTests.swift
+//  triplycountTests
+//
+//  Created by Paul Martinez on 13/04/2025.
+//
+
+import Testing
+@testable import triplycount
+
+struct triplycountTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
